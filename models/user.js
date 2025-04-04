b4e8a91f6dfb3bd32cfe1d900e7eb2fcecc195f6
kanajash <141270976+kanajash@users.noreply.github.com>
2025-04-04 23:37:56 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,49 +1,28 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false }
+}, { timestamps: true });
 
+// Hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-name:{
-    type:String,
-    required: true
-},
-email:{
-    type:String,
-    required: true
-},
-password:{
-    type:String,
-    required: true
-},
-isAdmin:{
-    type:Boolean,default: false
-}
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
+// Add method to compare entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-},{
-    timestamps: true
-})
-
-
-
-const userModel = mongoose.model("users",userSchema);
+const userModel = mongoose.model("users", userSchema);
 module.exports = userModel;
